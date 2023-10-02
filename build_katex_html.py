2a77f250a1f8bc1af697b622ce2a8ac94756cb1c
Jesse Stricker <git@jessestricker.de>
2023-10-02 12:37:11 +0200

    feat: create custom KaTeX integration

---
+++ build_katex_html.py
@@ -0,0 +1,38 @@
+# Copyright 2023 Jesse Stricker.
+# SPDX-License-Identifier: Apache-2.0
+
+from io import BufferedWriter
+from os import makedirs
+from pathlib import Path
+from shutil import copyfileobj
+from typing import Final
+from urllib.request import urlopen
+
+VERSION: Final = "0.16.8"
+SCRIPT_FILE_NAME: Final = "katex.min.js"
+OUTPUT_FILE_NAME: Final = f"katex.v{VERSION}.html"
+
+
+def build_url(file_name: str) -> str:
+    """Build an URL to the KaTeX asset file named ``file_name``."""
+    return f"https://cdn.jsdelivr.net/npm/katex@{VERSION}/dist/{file_name}"
+
+
+def download_to_end_of(file: BufferedWriter, url: str) -> None:
+    """Download the resource located at ``url`` and append it to ``file``."""
+    with urlopen(url) as response:
+        copyfileobj(response, file)
+
+
+def main() -> None:
+    output_file_path = Path(__file__).parent / ".cargo" / OUTPUT_FILE_NAME
+    makedirs(output_file_path.parent, exist_ok=True)
+
+    with output_file_path.open("wb") as output_file:
+        output_file.write(b"<script>\n")
+        download_to_end_of(output_file, build_url(SCRIPT_FILE_NAME))
+        output_file.write(b"\n</script>\n")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+//! # KaTeX example
+//!
+//! This documentation is an example of how to use mathematical notation.
+//!
+//! ## Inline math
+//!
+//! Lorem ipsum `$\int^a_b f(x)\,{\mathop\mathrm{d}}x$` dolor sit amet,
+//! consectetur `code` adipisicing elit.
+//!
+//! ## Display math
+//!
+//! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat placeat
+//! enim quisquam maiores obcaecati, odit, itaque aut iste ducimus fugit
+//! laboriosam ad sint autem repellendus accusamus veniam sunt nam architecto!
+//!
+//! ```math
+//! \mu = \frac{1}{n} \sum^n_{i=1} x_i
+//! ```
+//!
+//! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat placeat
+//! enim quisquam maiores obcaecati, odit, itaque aut iste ducimus fugit
+//! laboriosam ad sint autem repellendus accusamus veniam sunt nam architecto!
+//!
+//! ```
+//! println!("Hello, world!");
+//! ```
+
+/// You can even use inline math in the first paragraph summaries:
+/// `$c = \sqrt{a^2 + b^2}$`.
+///
+/// More details here.
+pub struct InlineMathInSummary;
